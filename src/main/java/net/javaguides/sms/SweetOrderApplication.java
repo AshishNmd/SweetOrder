a010873285f2a8fe912b1b6fa96e7826a7eb91c4
AshishNmd <143ashishnmd@gmail.com>
2024-05-02 13:06:27 +0530

    files add in repo

---
+++ src/main/java/net/javaguides/sms/SweetOrderApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetOrderApplication.class, args);
+	}
+
+}
